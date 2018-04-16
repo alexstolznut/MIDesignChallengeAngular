b405421064639c0dd6a3a687e8ef3a1d6c2087c0
Alex Stolzoff <amstolzo@ucsd.edu>
2018-04-16 02:04:34 -0700

    Hi

--- js/script.js
+++ js/script.js
@@ -1,49 +1,66 @@
-function appendChart(){
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
+function appendChart() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
                 'rgba(77, 182, 172, .5)'
             ],
-            borderColor: [
+                    borderColor: [
                 'rgba(77, 182, 172, .5)',
             ],
-            borderWidth: 1
+                    borderWidth: 1
         },
-                  {
+                {
                     label: '# of Votes 2',
-            data: [19, 3, 6, 7, 13, 9],
-            backgroundColor: [
+                    data: [19, 3, 6, 7, 13, 9],
+                    backgroundColor: [
                 'rgba(192,192,192,.5)'
             ],
-            borderColor: [
+                    borderColor: [
                 'rgba(192,192,192,.5)'
             ],
-            borderWidth: 1  
-                      
+                    borderWidth: 1
+
                   }]
-    },
-    options: {
-        legend: {
-            display:false
         },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
             }]
+            }
         }
-    }
-});
+    });
 }
 
 appendChart();
 
-//window.addEventListener('load', appendChart);
+var dialog = document.querySelector('dialog');
+var showDialogButton = document.querySelector('button');
+var purchaseDialog = document.getElementsByClassName('thanks');
 
+if (!dialog.showModal) {
+    dialogPolyfill.registerDialog(dialog);
+}
+showDialogButton.addEventListener('click', function () {
+    dialog.showModal();
+});
+dialog.querySelector('.close').addEventListener('click', function () {
+    dialog.close();
+});
+dialog.querySelector('.agree').addEventListener('click', function () {
+    dialog.close();
+    purchaseDialog.setAttribute('open');
+});
+
+//window.addEventListener('load', appendChart);
